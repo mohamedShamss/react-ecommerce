7058d2f68dea3c3ce648c5d1c1b27dd44c46865b
mohamedShamss <midosun@gmail.com>
2022-07-12 18:09:50 +0200

    Footer Component

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,6 +3,10 @@ const gulpSass = require('gulp-sass');
 const sass = gulpSass(require('sass'));
 
 gulp.task('sass' ,async function(){
+    gulp.watch('src/components/**/*.scss' ,async function(){
+
     gulp.src('src/sass/*.scss').pipe(sass()).pipe( gulp.dest('src/css') );
     gulp.src('src/components/**/*.scss').pipe(sass()).pipe( gulp.dest('src/css') );
+    
+    })
 })
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../../css/Footer/Footer.css'
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
